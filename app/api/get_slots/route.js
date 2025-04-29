a1e20011dda926688c0641d81d7265dfa47ec367
Nishanth Shankar <nishanthShankr@gmail.com>
2025-04-30 01:43:04 +0530

    Basic booking logic ready

--- app/api/get_slots/route.js
+++ app/api/get_slots/route.js
@@ -87,6 +87,7 @@ export async function GET(request) {
         const bookedEnd = dayjs(booked.end_time);
 
         return (
+          slotStart.isBefore(dayjs()) ||
           slotStart.isBetween(bookedStart, bookedEnd, "minute", "[]") ||
           slotEnd.isBetween(bookedStart, bookedEnd, "minute", "[]")
         );
