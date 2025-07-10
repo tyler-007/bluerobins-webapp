7c42a6cc4c91c7684519316d466a127a350ca33c
Nishanth Shankar <nishanthShankr@gmail.com>
2025-07-10 07:43:04 +0530

    Change prices

--- app/(private)/project-hub/[id]/edit/page.tsx
+++ app/(private)/project-hub/[id]/edit/page.tsx
@@ -66,11 +66,11 @@ const getValues = (project: any) => {
 
 const SELLING_PRICE_COST_MAP = {
   "8": {
-    selling_price: 1080,
+    selling_price: 1099,
     cost_price: 839,
   },
   "12": {
-    selling_price: 1530,
+    selling_price: 1649,
     cost_price: 900,
   },
 };
